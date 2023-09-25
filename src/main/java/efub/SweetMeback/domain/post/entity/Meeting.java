b50600add9e4ca6ff26d3cf979c6e3825b8c57d9
Minseo Cho <rove.minseo@gmail.com>
2023-09-26 00:04:21 +0900

    ✨feat: Enum 생성

--- src/main/java/efub/SweetMeback/domain/post/entity/Meeting.java
+++ src/main/java/efub/SweetMeback/domain/post/entity/Meeting.java
@@ -1,4 +1,12 @@
 package efub.SweetMeback.domain.post.entity;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum Meeting {
+    OFFLINE("OFFLINE", "대면"),
+    ONLINE("ONLINE", "비대면");
+
+    private final String label;
+    private final String detail;
 }
