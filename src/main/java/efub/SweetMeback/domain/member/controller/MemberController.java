125fc2778b6c62b2a296de840ef0e0fca0b353c9
hannah0226 <mohan0226@naver.com>
2023-10-10 23:22:47 +0900

    🔧fix: @RequestMapping 경로 수정 (/member -> /members)

--- src/main/java/efub/SweetMeback/domain/member/controller/MemberController.java
+++ src/main/java/efub/SweetMeback/domain/member/controller/MemberController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
