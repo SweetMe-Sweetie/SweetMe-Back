1ec339cc21bc410017845403e920c09a15d4531f
Minseo Cho <rove.minseo@gmail.com>
2023-10-24 21:46:19 +0900

    🔧 fix: 불필요한 기능 제거

--- src/main/java/efub/SweetMeback/domain/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package efub.SweetMeback.domain.order.repository;
-
-import efub.SweetMeback.domain.order.entity.PaymentOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<PaymentOrder, UUID> {
-}
