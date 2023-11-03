4dabf7f2080dc3539c891e7863b14d47c927ad5d
hannah0226 <mohan0226@naver.com>
2023-11-03 11:14:50 +0900

    ✨feat: 사용자 닉네임 수정 기능 추가

---
+++ src/main/java/efub/SweetMeback/domain/member/dto/MemberUpdateRequestDto.java
@@ -0,0 +1,12 @@
+package efub.SweetMeback.domain.member.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberUpdateRequestDto {
+    private String nickname;
+}
